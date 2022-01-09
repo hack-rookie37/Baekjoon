c05e00c0534459498f23270cde85f2f81629d5cb
Mootata <dbslt6872@gmail.com>
2022-01-09 22:07:42 +0900

    Mootata/1759

--- Mootata/2회차/2805.나무 자르기.py
+++ Mootata/2회차/2805.나무 자르기.py
@@ -1,6 +1,6 @@
 n, m = map(int, input().split()) # n 나무의 수, m 필요한 나무의 길이
 height = list(map(int, input().split())) # 각 나무의 높이
-start, end = 1, max(height) # 최소 높이 1 start, 집 앞 나무들 중 가장 높은 나무의 높이 end
+start, end = 0, max(height) # 벌목기의 첫 높이는 가장 긴 나무의 절반 높이
 height.sort() # 나무 높이순으로 정렬
 
 while start <= end:
@@ -11,9 +11,9 @@ while start <= end:
         if i > mid: # 나무의 높이가 mid보다 크면 잘라냄
             length += i - mid # 잘라낸 나무의 길이 더함
             
-    if length >= m: # 위의 연산을 통해 잘라낸 나무길이의 총합이 필요한 길이보다 크거나 같으면 오른쪽에서 찾음
+    if length >= m: # 필요한 길이보다 길거나 같으면 더 높게 설치
         start = mid + 1
-    else: # 작다면 왼쪽에서 찾음
+    else: # 짧다면 더 낮게 설치
         end = mid - 1
         
 print(end)
