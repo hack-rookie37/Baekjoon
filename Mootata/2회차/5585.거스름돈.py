00db003a5237abfa9c6aa1d96f499f1012cd6dd6
Mootata <dbslt6872@gmail.com>
2022-01-13 21:34:57 +0900

    Mootata/2178

--- Mootata/2회차/5585.거스름돈.py
+++ Mootata/2회차/5585.거스름돈.py
@@ -1,28 +1,10 @@
-pay = int(input(""))
+pay = int(input())
 change = 1000 - pay
 count = 0
+unit = [500, 100, 50, 10, 5, 1]
 
-if change >= 500:
-    count += 1
-    change -= 500
-
-if change >= 100:
-    count += change // 100
-    change -= (change // 100) * 100
-    
-if change >= 50:
-    count += change // 50
-    change -= (change // 50) * 50
-    
-if change >= 10:
-    count += change // 10
-    change -= (change // 10) * 10
-    
-if change >= 5:
-    count += change // 5
-    change -= (change // 5) * 5
-    
-if change >= 1:
-    count += change
+for coin in unit:
+    count += change // coin
+    change %= coin
 
 print(count)
