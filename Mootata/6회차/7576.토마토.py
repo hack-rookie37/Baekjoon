2efdbfb11734d530024a475d8d5cf0879fa8fb24
Mootata <dbslt6872@gmail.com>
2022-02-04 17:42:14 +0900

    comment: 주석 추가

--- Mootata/6회차/7576.토마토.py
+++ Mootata/6회차/7576.토마토.py
@@ -9,7 +9,7 @@ queue = deque()
 
 for i in range(n):
     for j in range(m):
-        if storage[i][j] == 1:
+        if storage[i][j] == 1: # 이미 익은 토마토부터 탐색을 시작하기 위해서 미리 큐에 담아줌
             queue.append((i, j))
 
 def bfs():
@@ -18,17 +18,17 @@ def bfs():
         
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and storage[nx][ny] == 0:
+            if 0 <= nx < n and 0 <= ny < m and storage[nx][ny] == 0: # 범위 안에 있고, 익지 않은 토마토 일 때
                 storage[nx][ny] = storage[x][y] + 1
                 queue.append((nx, ny))
-    return max(map(max, storage))
+    return max(map(max, storage)) # 가장 큰 값을 찾아서 리턴
 
 answer = bfs()
 
 for i in range(n):
     for j in range(m):
-        if storage[i][j] == 0:
+        if storage[i][j] == 0: # 아직 익지 않은 토마토가 있다면
             print(-1)
             exit(0)
             
-print(answer - 1)
+print(answer - 1) # 익은 토마토가 1부터 시작하므로 1을 빼줘야 함
