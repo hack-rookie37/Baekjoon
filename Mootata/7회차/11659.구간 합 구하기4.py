b2b9c65841fc2e6005d29136ca42db705994085f
Mootata <dbslt6872@gmail.com>
2022-02-08 16:30:07 +0900

    comment: 주석 추가

--- Mootata/7회차/11659.구간 합 구하기4.py
+++ Mootata/7회차/11659.구간 합 구하기4.py
@@ -12,4 +12,6 @@ for i in range(n):
 
 for _ in range(m):
     i, j = map(int, sys.stdin.readline().split())
-    print(prefix_sum[j] - prefix_sum[i - 1])
+    print(prefix_sum[j] - prefix_sum[i - 1]) # a ~ b까지의 합은 결국 첫번째 수부터 b번째 수 까지 더한 값에서
+                                             # 첫번째 수부터 a - 1번째 까지 수를 빼준 것과 같음 따라서 미리 모든 누적 합을 구해놓고
+                                             # 이것을 사용해서 구간 합을 구함
