7c780cc847a991c2e7fc13d7700e6935d4a7a544
Mootata <dbslt6872@gmail.com>
2022-02-15 20:27:10 +0900

    Mootata/2630

--- Mootata/8회차/2630.색종이 만들기.py
+++ Mootata/8회차/2630.색종이 만들기.py
@@ -1,9 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
 n = int(input()) # 한 변의 길이 n
 paper = [list(map(int, input().split())) for _ in range(n)]
+answer = [0, 0]
 
 def cut_papers(x, y, n):
     current_color = paper[x][y]
     for i in range(x, x + n):
         for j in range(y, y + n):
-            if current_color == paper[x][y]:
-                
+            if current_color != paper[i][j]:
+                n //= 2
+                cut_papers(x, y, n)
+                cut_papers(x, y + n, n)
+                cut_papers(x + n, y, n)
+                cut_papers(x + n, y + n, n)
+                # return
+    if current_color == 0:
+        answer[0] += 1
+    elif current_color == 1:
+        answer[1] += 1
+
+cut_papers(0, 0, n)
+print(*answer, sep = '\n')
