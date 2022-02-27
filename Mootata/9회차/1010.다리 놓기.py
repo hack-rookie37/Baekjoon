c27bc94dada978e4a3523cefbd28bdfcb2d71f9c
Mootata <dbslt6872@gmail.com>
2022-02-27 18:18:49 +0900

    refactor: 공식 직접 쓰는 대신 comb 사용

--- Mootata/9회차/1010.다리 놓기.py
+++ Mootata/9회차/1010.다리 놓기.py
@@ -1,8 +1,8 @@
-from math import factorial
+from math import comb
 
 for t in range(int(input())):
     n, m = map(int, input().split())
-    print(int(factorial(m) / (factorial(n) * factorial(m - n))))
+    print(comb(m ,n))
 
 # M개의 지역에 N개의 다리를 놓는 경우의 수
 # factorial[M] // (factorial[N] * factorial[M-N])
