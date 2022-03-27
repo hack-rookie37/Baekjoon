f6153d60640e9b8754675d68a4ebb2fd516afeae
Mootata <dbslt6872@gmail.com>
2022-03-27 19:27:29 +0900

    comment: Mootata/19638 주석 추가

--- Mootata/13회차/19638.센티와 마법의 뿅망치.py
+++ Mootata/13회차/19638.센티와 마법의 뿅망치.py
@@ -11,21 +11,21 @@ for i in range(n):
     heappush(heights, (-(int(input()))))
 
 while True:
-    if -heights[0] < h:
+    if -heights[0] < h: # 가장 큰 키를 가진 거인보다 센티가 클 때
         print('YES')
         print(count)
         break
-    elif t == 0:
+    elif t == 0: # 뿅망치 횟수 제한을 모두 소모했는데도, 위의 if문에 걸리지 않았으므로 실패
         print('NO')
         print(-heappop(heights))
         break
-    elif -heights[0] == 1:
+    elif -heights[0] == 1: # 가장 큰 거인의 키가 1인데도 맨 위의 if문에 걸리지 않았으므로, 센티의 키도 1이라는 뜻
         print('NO')
         print(-heappop(heights))
         break
     else:
         height = (-heappop(heights)) // 2
-        heappush(heights, -height)
+        heappush(heights, -height) # 거인의 키를 절반으로 낮추어 다시 우선순위 큐에 넣음
         t -= 1
         count += 1
 
