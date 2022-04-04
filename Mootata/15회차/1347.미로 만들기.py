314754a2705866fa79be36ff357ff55d4b6ba003
Mootata <dbslt6872@gmail.com>
2022-04-04 16:26:44 +0900

    comment: 1347 주석 추가

--- Mootata/15회차/1347.미로 만들기.py
+++ Mootata/15회차/1347.미로 만들기.py
@@ -16,14 +16,14 @@ rbx, rby = 50, 50
 
 for t in text:
     if t == 'R':
-        dir = (dir + 3) % 4
+        dir = (dir + 3) % 4 # F가 나왔을 때 이동할 방향을 오른쪽으로 90도 변경
     elif t == 'L':
-        dir = (dir + 1) % 4
+        dir = (dir + 1) % 4 # F가 나왔을 때 이동할 방향을 왼쪽쪽으로 90도 변경
     else:
         x += dx[dir]
         y += dy[dir]
-        ltx, lty = min(ltx, x), min (lty, y)
-        rbx, rby= max(rbx, x), max(rby, y)
+        ltx, lty = min(ltx, x), min (lty, y) # 출력할 부분 설정하기 위해서 가장 왼쪽위의 x, y 좌표와
+        rbx, rby= max(rbx, x), max(rby, y) # 가장 오른쪽 아래의 x, y 좌표를 구함
         maze[x][y] = '.'
 
 for i in range(ltx, rbx + 1):
