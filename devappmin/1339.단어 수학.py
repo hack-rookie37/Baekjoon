9e41d456ff775cb226bd3020f09320e62a087998
Kim Seung Hwan <dev.appmin@gmail.com>
2022-04-27 19:30:22 +0900

    refactor: devappmin/1339

--- devappmin/1339.단어 수학.py
+++ devappmin/1339.단어 수학.py
@@ -4,29 +4,24 @@ from heapq import heappush, heappop
 
 n = int(sys.stdin.readline())
 words = sorted( [sys.stdin.readline().rstrip() for _ in range(n)], key=lambda a: -len(a))
-word_list = []
 sorted_list = []
+exists = defaultdict(int)
 
 for word in words:
     length = len(word)
     
     for idx in range(length):
-        word_list.append((10**(length - idx - 1), word[idx]))
-    
+        exists[word[idx]] += 10**(length - idx - 1)
 
-exists = defaultdict(int)
 ans = 0
 val = 9
 
-for size, word in word_list:
-    exists[word] += -size
-
-for _, size in exists.items():
-    heappush(sorted_list, size)
+for word, size in exists.items():
+    heappush(sorted_list, -size)
 
 while sorted_list:
     item = heappop(sorted_list)
-    ans += -item * val
+    ans += -1 * item * val
     val -= 1
 
 print(ans)
