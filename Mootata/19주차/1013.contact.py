bad606038badbcd84f612bde75ba74f2efb3a86b
Mootata <dbslt6872@gmail.com>
2022-05-05 21:26:12 +0900

    comment: 주석 변경

--- Mootata/19주차/1013.contact.py
+++ Mootata/19주차/1013.contact.py
@@ -3,7 +3,7 @@ import re
 for t in range(int(input())):
     wave = input()
     
-    if re.fullmatch('(100+1+|01)+', wave): # match는 앞부분에서 정규식에 매치되는 
+    if re.fullmatch('(100+1+|01)+', wave): # match는 앞 부분만 체크하기 때문에 문자열 전체를 체크하는 fullmatch 사용
         print("YES")
     else:
         print("NO")
