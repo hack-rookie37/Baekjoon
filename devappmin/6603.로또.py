032ffcb0aa29719fc91de0f1fc24df6ab6e94173
Kim Seung Hwan <dev.appmin@gmail.com>
2022-05-13 00:26:04 +0900

    refactor: 6603

--- devappmin/6603.로또.py
+++ devappmin/6603.로또.py
@@ -4,15 +4,11 @@ from itertools import combinations
 lottos = []
 
 input_value = sys.stdin.readline().rstrip()
-
 while input_value != '0':
-    lottos.append(list(map(int, input_value.split())))
+    lottos.append(list(map(int, input_value.split()))[1:])
     input_value = sys.stdin.readline().rstrip()
 
 for lotto in lottos:
-    k = lotto[0]
-    s = lotto[1:]
-    for numbers in combinations(s, 6):
+    for numbers in combinations(lotto, 6):
         print(*numbers)
-
     print()
