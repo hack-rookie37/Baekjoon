a89e1c5a309d96255060cfbeea86ec2fbd5e38ed
Mootata <dbslt6872@gmail.com>
2022-06-17 06:04:58 +0900

    comment

--- Mootata/24회차/14889.스타트와 링크.py
+++ Mootata/24회차/14889.스타트와 링크.py
@@ -5,10 +5,10 @@ player = list(range(n))
 team = [list(map(int, input().split())) for _ in range(n)]
 answer = float('inf')
 
-for s in combinations(player, n // 2):
-    l = list(set(player) - set(s))
+for s in combinations(player, n // 2): # 팀이 만들어지는 경우의 수
+    l = list(set(player) - set(s)) # 그렇게 만들어진 팀원을 제외한 나머지로 구성된 팀
     start, link = 0, 0
-    for i, j in combinations(s, 2):
+    for i, j in combinations(s, 2): # 각각의 팀원끼리의 시너지 점수의 모든 합
         start += team[i][j]
         start += team[j][i]
     for i, j in combinations(l, 2):
