a2f69b3757758f768cff802a5c3415d409cf087d
Mootata <dbslt6872@gmail.com>
2022-06-29 16:23:09 +0900

    Mootata/11055

--- Mootata/26회차/11055.가장 큰 증가 부분 수열.py
+++ Mootata/26회차/11055.가장 큰 증가 부분 수열.py
@@ -5,7 +5,7 @@ dp = nums[:] # ex) dp[3] nums[0] ~ nums[3]의 가장 큰 증가 부분 수열의
 
 for i in range(1, n):
     for j in range(i):
-        if nums[j] < nums[i]:
+        if nums[j] < nums[i]: # nums[i]보다 작을 때만 더함 (증가 부분 수열)
             dp[i] = max(dp[i], dp[j] + nums[i])
 
 print(max(dp))
