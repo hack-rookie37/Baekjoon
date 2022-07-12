4ef10fbaf334ba5c33fa666399589e7b72c386e0
Mootata <dbslt6872@gmail.com>
2022-07-13 00:48:07 +0900

    Mootata/2156

--- Mootata/연습/2156.포도주 시식.py
+++ Mootata/연습/2156.포도주 시식.py
@@ -4,7 +4,11 @@ cups = []
 for i in range(n):
     cups.append(int(input()))
 
-dp = [cups[0], cups[0] + cups[1]] + [0 for _ in range(n - 2)]
+if n == 1:
+    print(cups[0])
+    exit(0)
+
+dp = [cups[0], cups[0] + cups[1]] + ([0] * n)
 
 for i in range(2, n):
     dp[i] = max(dp[i - 1], dp[i - 2] + cups[i], dp[i - 3] + cups[i] + cups[i - 1])
