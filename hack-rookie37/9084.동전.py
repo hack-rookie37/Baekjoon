d816445edba621ac88dd1ee27373618c950700a9
Junghoon Lee <dev.rookie37@gmail.com>
2022-07-22 16:57:30 +0900

    refactor: hack-rookie37/9084 save memory

--- hack-rookie37/9084.동전.py
+++ hack-rookie37/9084.동전.py
@@ -9,16 +9,11 @@ for _ in range(T):
     coins = [*map(int, input().split())]
     M = int(input())
 
-    dp = [[1] + [0] * M for _ in range(N)]
+    dp = [1] + [0] * M
 
-    for m in range(1, M + 1):
-        if m % coins[0] == 0:
-            dp[0][m] = 1
+    for i in coins:
+        for j in range(i, M + 1):
+            if j - i >= 0:
+                dp[j] += dp[j - i]
 
-    for n in range(1, N):
-        for m in range(1, M + 1):
-            dp[n][m] = dp[n - 1][m]
-            if coins[n] <= m:
-                dp[n][m] += dp[n][m - coins[n]]
-
-    print(dp[-1][-1])
+    print(dp[-1])
